3d353afd1a17d707e135f94b0fd343f0e431906b
yasutome <s144039@edu.hkg.ac.jp>
2018-01-10 10:03:15 +0000

    wall stop

--- scripts/wall_stop.py
+++ scripts/wall_stop.py
@@ -1,39 +1,40 @@
 #!/usr/bin/env python
-import rospy
+import rospy,copy
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
 from pimouse_ros.msg import LightSensorValues
 
-class WallStop():
+class WallTrace():
     def __init__(self):
         self.cmd_vel = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
-        rospy.Subscriber('/lightsensors', LightSensorValues, self.callback_sensors)
-        self.sensors = LightSensorValues()
 
-    def callback_sensors(self,messages):
-        self.sensors = messages
+        self.sensor_values = LightSensorValues()
+        rospy.Subscriber('/lightsensors', LightSensorValues, self.callback_lightsensors)
 
-    def decision(self,sensor_sum_all):
-        linear = 0.0 if sensor_sum_all >= 500 else 0.2
-        angular = 0.0
-        return linear, angular
+    def callback_lightsensors(self,messages):
+        self.sensor_values = messages
 
     def run(self):
         rate = rospy.Rate(10)
-        d = Twist()
+        data = Twist()
+
         while not rospy.is_shutdown():
-            d.linear.x, d.angular.z = self.decision(self.sensors.sum_all)
-            self.cmd_vel.publish(d)
+            data.linear.x = 0.2
+            data.angular.z = 0
+            if self.sensor_values.sum_all >= 500:
+                data.linear.x = 0
+                data.angular.z = 0
+
+            self.cmd_vel.publish(data)
             rate.sleep()
 
 if __name__ == '__main__':
     rospy.init_node('wall_trace')
+
     rospy.wait_for_service('/motor_on')
     rospy.wait_for_service('/motor_off')
-
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
-    if not rospy.ServiceProxy('/motor_on',Trigger).call().success:
-        rospy.logerr("motors are not empowered")
-        sys.exit(1)
+    rospy.ServiceProxy('/motor_on',Trigger).call()
 
-    WallStop().run()
+    w = WallTrace()
+    w.run()
